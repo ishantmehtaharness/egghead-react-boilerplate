1dad16fa30119b3600bc6a59db3c884f030bee39
ishantmehta <ishant@harness.io>
2020-06-24 01:21:36 +0530

    adds webpack-bundle-analyzer

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,12 @@
 const merge =  require('webpack-merge')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const baseconfig = require('./webpack.config.base')
 
 module.exports = merge(baseconfig, {
-    mode: 'production'
+    mode: 'production',
+    plugins: [new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        openAnalyzer: false,
+        reportFilename: 'bundle_sizes.html'
+    })]
 })
