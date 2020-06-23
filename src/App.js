644bb3b4d5cb7b66195e8474a7b69711a3692450
ishantmehta <ishant@harness.io>
2020-06-24 00:29:01 +0530

    adds @babel/plugin-proposal-class-properties

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import React from 'react'
 // import ReactDOM from 'react-dom'
 
 class App extends React.PureComponent {
+    state = {
+        count: 0
+    }
+
     render(){
-        return <h1>Hello, from React</h1>
+        return (
+            <div>
+                <h1>Hello, from React</h1>
+                <h2>Count: {this.state.count}</h2>
+                <button onClick={() => this.setState(state => ({count: state.count + 1}))}>+</button>
+                <button onClick={() => this.setState(state => ({count: state.count - 1}))}>-</button>
+            </div>
+        )
     }
 }
 
